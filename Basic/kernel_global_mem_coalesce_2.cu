2ecfe8cb78d0f9471c33caffd052e53b7549cec8
Dream_Blue <82110721+Dream-blue-wasTaken@users.noreply.github.com>
2025-11-03 19:36:58 +0530

    small change

--- Basic/kernel_global_mem_coalesce_2.cu
+++ Basic/kernel_global_mem_coalesce_2.cu
@@ -41,6 +41,18 @@
 #include <iostream>
 #include <iomanip>
 
+// const int cRow = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+//   const int cCol = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+
+//   // if statement is necessary to make things work under tile quantization
+//   if (cRow < M && cCol < N) {
+//     float tmp = 0.0;
+//     for (int i = 0; i < K; ++i) {
+//       tmp += A[cRow * K + i] * B[i * N + cCol];
+//     }
+//     C[cRow * N + cCol] = alpha * tmp + beta * C[cRow * N + cCol];
+//   }
+
 template <const uint BLOCKSIZE>
 __global__ void sgemm_global_mem_coalesce(int M, int N, int K, float alpha,
                                           const float *A, const float *B,
